e3bebbc043f1bfd21f264154782a2949cb9d38e3
david <etao.au@gmail.com>
2016-11-16 23:00:51 +1100

    create github pages

---
+++ assets/facebook-plugin/facebook-script.js
@@ -0,0 +1,2 @@
+
+(function(a){/^(http|ftp|https)/.test(window.location.href)&&a(".facebookPlaceholder").replaceWith("<div class='fb-comments' data-href='"+window.location.href+"' data-width='100%' data-numposts='"+a(".facebookPlaceholder").attr("data-numposts")+"'></div>")})(jQuery);
